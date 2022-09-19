fe9519d43fc97440775f80be35cb5127acb5b2e0
Simon Schaeffner <Simon Schaeffner>
2022-09-19 17:04:10 +0000

    use monospace font for clock, fix timezone

--- providers/clock_provider.py
+++ providers/clock_provider.py
@@ -1,10 +1,11 @@
 from dis import show_code
 import time
-import datetime
+import pytz
 import numpy as np
+from datetime import datetime
 from PIL import Image
 from PIL import ImageFont
-from PIL import ImageDraw 
+from PIL import ImageDraw
 from .text_provider import TextProvider
 
 from  providers.utils import color_hex2triplet, color_mult
@@ -14,9 +15,10 @@ from  providers.utils import color_hex2triplet, color_mult
 class ClockProvider(TextProvider):
     def __init__(self, matrix, fps=5) -> None:
         super().__init__(matrix, fps)
-        self.font = ImageFont.truetype("fonts/digital-7.ttf", size=30)
+        self.font = ImageFont.truetype("fonts/FiraCode-Regular.otf", size=20)
         self.textColor = (150,150,20)
         self.titleColor = (100,200,200)
+        self.tz = pytz.timezone('Europe/Berlin')
 
     def displayContent(self, t) -> None:
         starttime = time.time()
@@ -30,6 +32,7 @@ class ClockProvider(TextProvider):
         image = Image.new('RGB', self.matrix.getSize(), (0,0,0))
         draw = ImageDraw.Draw(image)
         (x,y) = self.matrix.getCenter()
-        y = y - round(self.font.size/2)
-        super().centeredtext(draw, (x,y), time.strftime("%H:%M:%S"))
+        y = y - round(self.font.size/2) - 4
+
+        super().centeredtext(draw, (x,y), datetime.now(self.tz).strftime("%H:%M:%S"))
         return image
